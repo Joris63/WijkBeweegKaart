0cbc81b70e5cb29d37258b288234e91ffd641538
Bohlken,Joep J <joepbohlken@gmail.com>
2022-11-30 15:38:55 +0100

    wip levelselection

--- WebApp/src/App.js
+++ WebApp/src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
+import "./styles/level-selection.scss";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import SurveyQuestions from "./components/survey/SurveyQuestions";
-import LevelButton from "./components/levelSelection/LevelButton";
+import LevelButton from "./components/level-selection/LevelSelectionItem";
 
 function App() {
   return (
---
+++ WebApp/src/components/level-selection/LevelSelectionItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function LevelSelectionItem() {
+  return (
+    <div>
+      <button className="level_button">
+        <div className="level_name">Level name</div>
+        <div className="level_status">
+          <i class="fa-regular fa-star"></i>
+        </div>
+        <div className="level_reward_cointainer">
+          <div className="element">100</div>
+          <div className="level_reward_icon element">
+            <i class="fa-regular fa-coin"></i>
+          </div>
+        </div>
+      </button>
+    </div>
+  );
+}
