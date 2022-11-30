a7cad6e4e5b12e4989d69a2a3267aa8c61f29596
Joris Kamminga <joris.kamminga@gmail.com>
2022-11-30 21:08:28 +0100

    Added some boilerplate login things

--- WebApp/src/App.js
+++ WebApp/src/App.js
@@ -1,22 +1,36 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Routes,
+  Route,
+} from "react-router-dom";
+import RequireAuth from "./components/auth/RequireAuth";
 
+// Import pages
+import AuthPage from "./pages/AuthPage";
+import LevelSelectPage from "./pages/LevelSelectPage";
+import MapViewerPage from "./pages/MapViewerPage";
+import SurveyPage from "./pages/SurveyPage";
+
+// Import stylesheets
 import "./styles/index.scss";
+import "./styles/survey.scss";
+import "./styles/level-select.scss";
 
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SurveyPage />} />
+        <Route path="/login" element={<AuthPage />} />
+        <Route element={<RequireAuth />}>
+          <Route path="/levels" element={<LevelSelectPage />} />
+          <Route path="/map" element={<MapViewerPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
-/*
-
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<SurveyQuestions />} />
-        </Routes>
-      </Router>
-
-      */
--- WebApp/src/components/level/LevelButton.js
+++ WebApp/src/components/level/LevelButton.js
@@ -1,4 +1,3 @@
-import React from "react";
 
 const LevelButton = () => {
     return null;
