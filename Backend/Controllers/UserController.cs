bdfffc2190164c3903c2f4074449a09c8dc4359d
TimDus <timvddussen@gmail.com>
2022-12-06 15:58:36 +0100

    Create UserController.cs

---
+++ Backend/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using Backend.Logic;
+using Backend.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("User")]
+    public class UserController : Controller
+    {
+        private readonly UserLogic _logic;
+        public UserController(UserLogic logic)
+        {
+            _logic = logic;
+        }
+
+        [HttpGet]
+        [Route("{Id}")]
+        public IActionResult GetUserById(int Id)
+        {
+            try
+            {
+                UserViewModel map = _logic.GetUserById(Id);
+
+                return Ok(map);
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        [Route("Save")]
+        public IActionResult SaveUser(UserViewModel user)
+        {
+            try
+            {
+                _logic.SaveUser(user);
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, ex);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(new { User, ex });
+            }
+            catch (ArgumentNullException ex)
+            {
+                return BadRequest(new { User, ex });
+            }
+
+            return Ok(User);
+        }
+    }
+}
