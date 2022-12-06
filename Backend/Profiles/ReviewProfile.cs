7007dd0a53a0fb60764a8b50b7ea8e5ead58aff1
TimDus <timvddussen@gmail.com>
2022-12-06 10:59:35 +0100

    Class Profiles

---
+++ Backend/Profiles/ReviewProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Backend.Models.ViewModels;
+using Backend.Models.BusinessModels;
+using Backend.Models.DTOModels;
+
+namespace Backend.Profiles
+{
+    public class ReviewProfile : Profile
+    {
+        public ReviewProfile()
+        {
+            CreateMap<ReviewViewModel, Review>().ReverseMap();
+            CreateMap<ReviewDTO, Review>().ReverseMap();
+        }
+    }
+}
