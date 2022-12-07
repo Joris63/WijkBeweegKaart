2e2531ef78bca8cd1ecb3d6501d5660050584b4b
Githubberic <434092@student.fontys.nl>
2022-12-07 10:02:46 +0100

    Survey (almost) done

--- WebApp/src/services/SurveyService.js
+++ WebApp/src/services/SurveyService.js
@@ -1,12 +1,16 @@
 import http from "../http-common";
 
 const getAll = () => {
-    return http.get("/v3/surveys/509691265/details")
+    return http.get("surveys/509691265/details")
+  }
+  const post = (data) => {
+    return http.post("collectors/448832583/responses", data)
   }
   
 
 const functions = {
     getAll,
+    post
   }
   
   export default functions;
