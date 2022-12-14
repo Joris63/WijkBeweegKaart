c022514e8a19a5812a212a280078b49de5d6e38e
Joris Kamminga <joris.kamminga@gmail.com>
2022-12-14 10:31:42 +0100

    FInished communication test between Unity and react

--- WebApp/src/components/map/MapViewer.js
+++ WebApp/src/components/map/MapViewer.js
@@ -1,12 +1,42 @@
+import { useCallback, useEffect, useState } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 const MapViewer = () => {
-  const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
+  const [testText, setTestText] = useState("");
+  const {
+    unityProvider,
+    loadingProgression,
+    isLoaded,
+    sendMessage,
+    addEventListener,
+    removeEventListener,
+  } = useUnityContext({
     loaderUrl: "/build/map-editor.loader.js",
     dataUrl: "/build/map-editor.data",
     frameworkUrl: "/build/map-editor.framework.js",
     codeUrl: "/build/map-editor.wasm",
   });
+
+  function UpdateText() {
+    let date = new Date();
+
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    sendMessage("GameController", "UpdateTextField", `${day}-${month}-${year}`);
+  }
+
+  const handleTest = useCallback((name) => {
+    setTestText(name);
+  }, []);
+
+  useEffect(() => {
+    addEventListener("Test", handleTest);
+    return () => {
+      removeEventListener("Test", handleTest);
+    };
+  }, [addEventListener, removeEventListener, handleTest]);
 
   return (
     <>
@@ -15,7 +45,7 @@ const MapViewer = () => {
           <div className="bar"></div>
         </div>
       )}
-      <div className="map">
+      <div className="map" onClick={UpdateText}>
         <Unity
           unityProvider={unityProvider}
           style={{
@@ -24,6 +54,15 @@ const MapViewer = () => {
             height: 297,
           }}
         />
+      </div>
+      <div
+        style={{
+          position: "fixed",
+          left: "50%",
+          transform: "translateX(-50%)",
+        }}
+      >
+        {testText}
       </div>
     </>
   );
