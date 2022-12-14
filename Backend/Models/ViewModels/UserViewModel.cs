0728ef3da69a30cb2e5861473c7688a86040982a
Max Dussen <maxvdd76@gmail.com>
2022-12-14 11:45:05 +0100

    user nullable fields

--- Backend/Models/ViewModels/UserViewModel.cs
+++ Backend/Models/ViewModels/UserViewModel.cs
@@ -3,8 +3,8 @@
     public class UserViewModel
     {
         public int Id { get; set; }
-        public MapViewModel[] CreatedMaps { get; set; }
-        public ICollection<DonationViewModel> MadeDonations { get; set; }
+        public MapViewModel[]? CreatedMaps { get; set; }
+        public ICollection<DonationViewModel>? MadeDonations { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string? Email { get; set; }
